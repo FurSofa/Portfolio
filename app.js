c0a3e3682b5f9b08e6a81b1021601589902c6ccc
fursofa <cml6497@gmail.com>
2021-01-26 21:47:24 -0500

    -init

---
+++ app.js
@@ -0,0 +1,49 @@
+import projects from "./projectsData";
+
+function generateProjectCards(projectsArray) {
+    console.log('ini projects cards');
+  // get repos from github
+  // destructure into
+
+  projectsArray.forEach(({ name, image, info, link }) => {
+    const projectsContainer = document.getElementById("projects-container");
+
+    let projectCard = document
+      .createElement("div")
+      .setAttribute("class", "card");
+
+    let projectImage = document.createElement("img");
+    projectImage.setAttribute("class", "projectImage");
+    projectImage.setAttribute("src", "");
+    projectImage.setAttribute("alt", "project Image");
+
+    let projectName = document
+      .createElement("h4")
+      .setAttribute("class", "projectName");
+    let projectInfo = document
+      .createElement("p")
+      .setAttribute("class", "projectInfo");
+    let projectLink = document
+      .createElement("a")
+      .setAttribute("class", "projectLink");
+
+    projectName.innerHTML = name;
+    projectName.innerHTML = image;
+    projectName.innerHTML = info;
+    projectName.innerHTML = link;
+
+    projectCard.appendChild(projectImage);
+    projectCard.appendChild(projectName);
+    projectCard.appendChild(projectInfo);
+    projectCard.appendChild(projectLink);
+    projectsContainer.appendChild(projectCard);
+  });
+  console.log('complete projects cards');
+}
+function main() {
+    console.log('init');
+  generateProjectCards(projects);
+  console.log('complete');
+}
+
+main();
---
+++ projectsData.js
@@ -0,0 +1,9 @@
+ const projects = [
+    {
+        name:'Test Project', // project title
+        image: 'https://pbs.twimg.com/profile_images/1342768807891378178/8le-DzgC.jpg', // src/ alt value
+        info: 'This is how qualified works', // info about project
+        link: 'https://github.com/FurSofa/personal-website', // link to site/repo
+    },
+]
+export default projects;
