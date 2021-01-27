183a604f621c2d15c8389db3b5ea16e7376b1aa6
fursofa <cml6497@gmail.com>
2021-01-26 22:01:23 -0500

    ini2

--- projectsData.js
+++ projectsData.js
@@ -1,4 +1,4 @@
- const projects = [
+export const projects = [
     {
         name:'Test Project', // project title
         image: 'https://pbs.twimg.com/profile_images/1342768807891378178/8le-DzgC.jpg', // src/ alt value
@@ -6,4 +6,3 @@
         link: 'https://github.com/FurSofa/personal-website', // link to site/repo
     },
 ]
-export default projects;
